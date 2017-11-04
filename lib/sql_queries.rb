ddea1737b1d8e9408e596f070bf4a7e4ba77ab5d
Susan Fowler <infopro94@gmail.com>
2017-11-04 22:44:00 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -26,7 +26,7 @@ end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
   "SELECT users.name, SUM(pledges.amount) FROM users JOIN pledges ON users.id = pledges.user_id GROUP BY users.name ORDER BY pledges.amount ;"
-#select users.name and sum(pledges) group by users.name, sort by amount then users.name.
+#select users.name and sum(pledges) group by users.name, sort by amount.
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
