ed43d2c80d3b4472f59b6f19d11bf052ba221fdf
Susan Fowler <infopro94@gmail.com>
2017-11-04 22:38:19 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -22,7 +22,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-  "SELECT users.name, SUM(pledges.amount) FROM users JOIN pledges ON user.id = pledges.user_id GROUP BY pledges.amount, users.name;"
+  "SELECT users.name, SUM(pledges.amount) FROM users JOIN pledges ON users.id = pledges.user_id GROUP BY pledges.amount, users.name;"
 #select users.name and sum(pledges) group by users.name, sort by amount then users.name. ORDER BY = sort by specific columns. GROUP BY = summarize unique combinations of columns values
 end
 
